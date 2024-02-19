8cab0e4223bb94db998618fbc7b3be094513075d
mashfikur-bdcalling <mashfikurbdcalling@gmail.com>
2024-02-19 13:04:24 +0600

    smooth scrolling added

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Lenis from "@studio-freight/lenis";
 
 const Layout = () => {
+  const lenis = new Lenis();
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
   return (
     <div>
       <Outlet></Outlet>
